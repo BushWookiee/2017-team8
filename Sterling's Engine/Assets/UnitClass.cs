9f5655013aea1308f9d059c21d869ccd4d911eea
BushWookiee <jwwolosc@mtu.edu>
2017-10-08 12:43:00 -0400

    Created UnitClass.cs
    Created general class decsription file. This file will help with
    creating more specific units by making it easier to give them numerical
    values for different stats

---
+++ Sterling's Engine/Assets/UnitClass.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitClass{
+    private string unitClassName;
+
+    //Stats
+    private int health;
+    private int range;
+    private int movement;
+    private int damage;
+
+    public string UnitClassName
+    {
+        get { return unitClassName; }
+        set { unitClassName = value; }
+    }
+    
+    public int Health
+    {
+        get { return health; }
+        set { health = value; }
+    }
+
+    public int Range
+    {
+        get { return range; }
+        set { range = value; }
+    }
+
+    public int Movement
+    {
+        get { return movement; }
+        set { movement = value; }
+    }
+
+    public int Damage
+    {
+        get { return damage; }
+        set { damage = value; }
+    }
+}
